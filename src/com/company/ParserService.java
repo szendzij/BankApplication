4da8947c422ad93ef0fb0e03be46c8d3df67b7fd
monikanowak0212 <monika.nowak0212@gmail.com>
2022-11-24 14:54:40 +0100

    working on parserService

--- src/com/company/ParserService.java
+++ src/com/company/ParserService.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class ParserService {
     public void checkPhoneNumber() {
-
+        //will chcek if entered value is in proper format
     }
+
+    public void checkBalance() {
+        //will chcek if entered value is in proper format
+    }
+
 }
